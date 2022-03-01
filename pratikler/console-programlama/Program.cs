5a4ce83242db7c31b88d0553880635d136ebc533
eroldalk <erol_dalkilic@hotmail.com>
2022-03-01 21:53:47 +0300

    terminalden eklendi

---
+++ pratikler/console-programlama/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program 
+    {
+        public static void Main (string[] args)
+        {
+            System.Console.WriteLine("hello world");
+            System.Console.WriteLine("İsminizi Giriniz:");
+            string name = Console.ReadLine();
+            System.Console.WriteLine("Soyadınızı Giriniz:");
+            string surname = Console.ReadLine();
+
+            System.Console.WriteLine("Merhaba" + name + " " + surname);
+
+            return;
+
+        }
+    }
+}
